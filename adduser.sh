e73036c4a01b7822a55260dde63631153af63fa9
shinochin <kazutoshi.shinoda0516@gmail.com>
2020-07-09 08:18:54 +0000

    update

--- adduser.sh
+++ adduser.sh
@@ -6,3 +6,5 @@ sudo chown $1:users /home/$1/.ssh
 sudo chmod 700 /home/$1/.ssh
 sudo chown $1:$1 /home/$1/.ssh/authorized_keys
 sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo mkdir /data/$1/
+sudo chown $1:$1 /data/$1/
