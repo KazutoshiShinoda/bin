9b1e57ae7d89379a6bd9e17b3537a730386e42e4
shinochin <kazutoshi.shinoda0516@gmail.com>
2021-11-25 23:08:50 +0900

    add

---
+++ adduser_wid.sh
@@ -0,0 +1,12 @@
+sudo adduser $1
+sudo groupmod -g $3 $1
+sudo usermod -u $3 $1
+sudo mkdir /home/$1/.ssh
+sudo touch /home/$1/.ssh/authorized_keys
+sudo echo $2 > /home/$1/.ssh/authorized_keys
+sudo chown $1:users /home/$1/.ssh
+sudo chmod 700 /home/$1/.ssh
+sudo chown $1:$1 /home/$1/.ssh/authorized_keys
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo mkdir /data/$1/
+sudo chown $1:$1 /data/$1/
