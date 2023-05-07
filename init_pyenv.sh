33341c31521aa9d44d273eaa5e74c689d1052f9d
Kazutoshi Shinoda <kazutoshi.shinoda0516@gmail.com>
2023-05-07 17:57:55 +0900

    Update init_pyenv.sh

--- init_pyenv.sh
+++ init_pyenv.sh
@@ -1,9 +1,10 @@
+# For Mac
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $HOME/.bash_profile
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> $HOME/.bash_profile
-echo 'eval "$(pyenv init -)"' >> $HOME/.bash_profile
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $HOME/.zprofile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> $HOME/.zprofile
+echo 'eval "$(pyenv init -)"' >> $HOME/.zprofile
 
 git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.bash_profile
+echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.zprofile
 
-source ~/.bash_profile
+source ~/.zprofile
